ac3d3fc00d5a414d0f05039e4f27b871970908f5
matow24 <matow224@gmail.com>
2025-05-21 00:29:42 +0200

    PD 2||Cmax

--- rozwiazanie.cpp
+++ rozwiazanie.cpp
@@ -8,8 +8,6 @@ problem::problem(std::vector<zadanie> zWP, int lM ):zadaniaWProblemie(zWP), licz
         maszyny.push_back(m);
     }
     liczbaZadan = zadaniaWProblemie.size();
-
-
 };
 
 
@@ -41,7 +39,7 @@ int rozwiazanie::countCzasWykonania(std::vector<maszyna> m){
 //    dla kazdej maszyny policz czas wykonywania i zwroc najwiekszy
     kryterium=0;
 
-    for(int l=0; l<m.size();l++)
+    for(int l=0; l<m.size(); l++)
     {
 //        policz czas wykonywania
         if(kryterium<m.at(l).getSumaCzasuZadan())
@@ -53,17 +51,16 @@ int rozwiazanie::countCzasWykonania(std::vector<maszyna> m){
 }
 
 
-
 int rozwiazanie::getIDWolnejMaszyny() {
 
     int ID;
     int MinCzas=INT16_MAX;
-    for(int i =0; i<P.getMaszyny().size();i++)
+    for(int i =0; i<P.getLiczbaMaszyn();i++)
     {
-        if(P.getMaszyny().at(i).getSumaCzasuZadan() == 0)
+        if(P.getMaszyna(i).getSumaCzasuZadan() == 0)
             return i;
-        if(P.getMaszyny().at(i).getSumaCzasuZadan()<MinCzas) {
-            MinCzas = P.getMaszyny().at(i).getSumaCzasuZadan();
+        if(P.getMaszyna(i).getSumaCzasuZadan() < MinCzas) {
+            MinCzas = P.getMaszyna(i).getSumaCzasuZadan();
             ID = i;
         }
     }
@@ -78,8 +75,8 @@ void rozwiazanie::algorytmLPT()
     for (int i = 0; i < P.getLiczbaZadan(); i++)
     {
         int idMaszyny = getIDWolnejMaszyny();
-        zadanie zad = P.getProblem().at(i);
-        P.getMaszyny().at(idMaszyny).dodajZadanie(zad);
+        zadanie zad = P.getZadanie(i);
+        P.getMaszyna(idMaszyny).dodajZadanie(zad);
     }
 
     countCzasWykonania(P.getMaszyny());
@@ -91,27 +88,33 @@ void rozwiazanie::algorytmLSA()
     {
         int idMaszyny = getIDWolnejMaszyny();
         zadanie zad = P.getZadanie(i);
-        P.getMaszyny().at(idMaszyny).dodajZadanie(zad);
+        P.getMaszyna(idMaszyny).dodajZadanie(zad);
     }
 
     countCzasWykonania(P.getMaszyny());
 }
 
-//void rozwiazanie::programowanieDynamiczne_P2_Cmax(){
-//    int Kl = P.getSumaPj()/2 +1;
-//    int n = P.getLiczbaZadan();
-//
-//    int T[n+1][Kl] = {};
-//
-//    // kolumna 0 = zadanie 0 o zerowym czasie wykonywania
-//    for(int i=0; i <= n; i++){
-//        T[i][0] = 1;
-//    }
-//
-//    for(int j=1; j <= n; j++)
-//        for(int k=1; k < Kl; k++)
-//            if( (T[j-1][k]==1) || (( k >= pj )&&( T[j-1][k-pj]==1 )) )
-//                T[j][k]=1;
-//
-//    // tu trzeba zrobić odszyfrowywanie
-//}
+void rozwiazanie::programowanieDynamiczne_P2_Cmax(){
+   int Kl = P.getSumaPj()/2 +1;
+   int n = P.getLiczbaZadan();
+
+   bool T[n+1][Kl] = {};
+
+   // kolumna 0 = zadanie 0 o zerowym czasie wykonywania
+   for(int i=0; i <= n; i++){
+       T[i][0] = 1;
+   }
+
+   for(int j=1; j <= n; j++)
+       for(int k=1; k < Kl; k++)
+           T[j][k] = (T[j-1][k]==1) || ( k >= pj && T[j-1][k-pj]==1 );
+
+    int maxK = 0;
+    for(int k = Kl-1; k >= 0; k--){
+        if(T[n][k]){
+            maxK = k;
+            break;
+        }
+    }
+    kryterium = std::max(maxK, getSumaPj() - maxK);
+}
