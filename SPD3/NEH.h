f6e65a1dc16d86c2fdf41895cfad1cdd964ac503
Adam Szczepaniak <275484@studnet.pwr.edu.pl>
2025-05-28 12:48:31 +0200

    Po zajeciach 28 maja

--- SPD3/NEH.h
+++ SPD3/NEH.h
@@ -5,15 +5,53 @@
 #ifndef SPD3_NEH_H
 #define SPD3_NEH_H
 
+#include <vector>
 /*
  * Rozpoczyna on działanie od pewnego rozwiązania początkowego π_init, które określa kolejność zadań.
  * W wersji dla klasycznego problemu zadania są uszeregowane zgodnie z niemalejącą sumą czasów wykonywania operacji.
  * Następnie zadania z π_init,są kolejno wstawiane do rozwiązania końcowego π^*,
  * na taką pozycję, aby zminimalizować wartość kryterium Cmax(π^*).
+ *
+ *
+  std::vector<int> suma_czasow(liczba_zadan, 0);
+  for(int j=0;j<liczba_zadan;j++){
+      for(int m=0; m<liczba_maszyn; ++m){
+          suma_czasow[j] += p[j][m];
+      }
+  }
+
+2. Posortuj zadania według suma_czasów[j] malejąco → lista_zadan_desc
+
+ harmonogram.push(lista_zadan_desc[0]);
+
+ int nowy_Cmax = 0;
+ for(int i=1;i<liczba_zadan;i++){
+       Cmax = INTMAXVALUE;
+       std::vector<int> nowy_harmonogram;
+       std::vector<string>::iterator k;
+       for(k = harmonogram.begin(); k != harmonogram.end(); ++k){
+
+           nowy_harmonogram := wstaw lista_zadan_desc[i] na pozycję k w harmonogramie
+
+           nowy_Cmax = oblicz_Cmax(nowy_harmonogram, p);
+           if(nowy_Cmax < Cmax){
+               Cmax = nowy_Cmax;
+               harmonogram = nowy_harmonogram;
+           }
+           nowy_harmonogram = harmonogram;
+       }
+    }
+
+5. Zwróć harmonogram i jego Cmax
+
  */
 class NEH {
-
+    int liczba_zadan;
+    int liczba_maszyn;
+    int sumapj;
+    int Cmax;
+    //std::vector<std::vector<int>> p(liczba_zadan, std::vector<int>(liczba_maszyn));
+    std::vector<int>harmonogram; //kolejnosc zadan
 };
-
 
 #endif //SPD3_NEH_H
